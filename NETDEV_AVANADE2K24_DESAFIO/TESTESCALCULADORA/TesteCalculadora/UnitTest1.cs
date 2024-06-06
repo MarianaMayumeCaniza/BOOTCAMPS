871e24844abe98d98d56ab6480acf382ae0eb4b5
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2024-06-06 11:48:32 -0300

    update

--- NETDEV_AVANADE2K24_DESAFIO/TESTESCALCULADORA/TesteCalculadora/UnitTest1.cs
+++ NETDEV_AVANADE2K24_DESAFIO/TESTESCALCULADORA/TesteCalculadora/UnitTest1.cs
@@ -1,10 +1,15 @@
+using DESAFIOCALCULADORA.Models;
+
 namespace TesteCalculadora;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(1,2,3) ]
+    public void Test1(int num1, int num2, int resultado )
     {
-
+        CalculadoraSimples calc = new CalculadoraSimples();
+        int testeSomar = calc.somar(num1,num2);
+        Assert.Equal(resultado, testeSomar);
     }
 }
