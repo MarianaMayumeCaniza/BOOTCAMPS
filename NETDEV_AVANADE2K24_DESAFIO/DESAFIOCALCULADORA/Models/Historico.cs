6e0435f07dc34ac1c70ae6e4eb94460d42499c0c
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2024-06-06 11:26:40 -0300

    update

---
+++ NETDEV_AVANADE2K24_DESAFIO/DESAFIOCALCULADORA/Models/Historico.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DESAFIOCALCULADORA.Models
+{
+    public class Historico
+    {
+        public List<string> historico(){
+            return new List<string>();;
+        }
+
+    }
+}
