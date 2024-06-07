0f51768b7338748abad4dd760c85d740af310f7f
MarianaMayumeCaniza <caniza.mariana@unifesp.br>
2024-06-07 10:28:22 -0300

    Update CalculadoraSimples.cs

--- NETDEV_AVANADE2K24_DESAFIO/DESAFIOCALCULADORA/Models/CalculadoraSimples.cs
+++ NETDEV_AVANADE2K24_DESAFIO/DESAFIOCALCULADORA/Models/CalculadoraSimples.cs
@@ -8,19 +8,19 @@ namespace DESAFIOCALCULADORA.Models
     public class CalculadoraSimples
     {
         public int somar(int num1, int num2){
-            return 0;
+            return num1+num2;
         }
 
         public int subtrair(int num1, int num2){
-            return 0;
+            return num1-num2;
         }
 
         public int multiplicar(int num1, int num2){
-            return 0;
+            return num1*num2;
         }
 
         public int dividir(int num1, int num2){
-            return 0;
+            return num1/num2;
         }
     }
 }
